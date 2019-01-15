fcfa7826a82e3f149712bc8e779e9c730502b4c5
Shivank Agarwal <shivankagarwal@Shivank-Agarwal594-MPR00190.local>
2019-01-15 20:36:10 +0530

    Implementation of printing duplicate element in o(n) time complexity and constant extra space.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,21 @@
-import UIKit
 
-var str = "Hello, playground"
+var inputArray = [1,2,3,1,3,6,6]
+
+// First check all the values that are
+// present in an array then go to that
+// values as indexes and increment by
+// the size of array
+let count = inputArray.count
+for i in 0..<count{
+    let index = inputArray[i]%count
+    inputArray[index] += count
+}
+
+// Now check which value exists more
+// than once by dividing with the size
+// of array
+for i in 0..<count{
+    if inputArray[i]/count > 1{
+        print("%d is duplicate element", i)
+    }
+}
